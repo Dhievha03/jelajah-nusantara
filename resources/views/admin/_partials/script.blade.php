32cb01ca6ec24e8aa30c85c6f6911298bce48b63
aryadhievha03 <aryadhievha03@gmail.com>
2023-11-22 16:19:40 +0700

    [AD - #CSD10]

--- resources/views/admin/_partials/script.blade.php
+++ resources/views/admin/_partials/script.blade.php
@@ -1,18 +1,18 @@
 <!-- Bootstrap core JavaScript-->
-<script src="{{ asset('template_admin/vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('template_admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('template-admin/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('template-admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="{{ asset('template_admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('template-admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="{{ asset('template_admin/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('template-admin/js/sb-admin-2.min.js') }}"></script>
 
 <!-- Page level plugins -->
-<script src="{{ asset('template_admin/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('template-admin/vendor/chart.js/Chart.min.js') }}"></script>
 
 <!-- Page level custom scripts -->
-<script src="{{ asset('template_admin/js/demo/chart-area-demo.js') }}"></script>
-<script src="{{ asset('template_admin/js/demo/chart-pie-demo.js') }}"></script>
+<script src="{{ asset('template-admin/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('template-admin/js/demo/chart-pie-demo.js') }}"></script>
 
 @yield('script')
