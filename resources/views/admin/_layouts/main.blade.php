12ecae8c8986e9bfc4854a8aefd959600c8ac490
aryadhievha03 <aryadhievha03@gmail.com>
2023-11-24 00:44:14 +0700

    [AD - #CSD11]

--- resources/views/admin/_layouts/main.blade.php
+++ resources/views/admin/_layouts/main.blade.php
@@ -50,16 +50,12 @@
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
+                <div class="modal-body">
+                    <h5 class="modal-title">Apakah Anda yakin ingin keluar?</h5>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Batalkan</button>
+                    <a class="btn btn-primary" href="{{ route('admin.logout') }}">Logout</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProvinsiController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/dashboard', [DashboardController::class, 'index']);
-
 Route::get('/provinsi', [ProvinsiController::class, 'index']);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    // Auth
+    Route::get('/login', [AuthController::class, 'index'])->middleware('guest')->name('login');
+    Route::post('/login/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
+
+    Route::group(['middleware' => ['auth:admin']], function() {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+});
