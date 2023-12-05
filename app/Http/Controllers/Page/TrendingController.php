8b8dffff128273d420c1522510649fbdbe901edc
auliannisafjra <auliarara03@gmail.com>
2023-12-05 16:35:39 +0700

    [AAF - #CSD31]

--- app/Http/Controllers/Page/TrendingController.php
+++ app/Http/Controllers/Page/TrendingController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\page;
 
 use App\Http\Controllers\Controller;
+use App\Models\Provinsi;
+use App\Models\Wisata;
+use App\Models\Trending;
 use Illuminate\Http\Request;
 
 class TrendingController extends Controller
 {
     public function index()
     {
-        return view('page.trending');
+        $cards = Wisata::select('id', 'nama_wisata', 'deskripsi', 'foto')->where('status', '=', '1')->get();
+        $select = Provinsi::get();
+        return view('page.trending', compact('cards', 'select'));
+    }
+
+    public function show(Trending $cards, $id)
+    {
+        $cards = Wisata::findOrFail($id);
+
+        return view('page.detail', compact('cards'));
     }
 }
--- resources/views/page/trending.blade.php
+++ resources/views/page/trending.blade.php
@@ -2,177 +2,49 @@
 @section('title', 'Trending')
 
 @section('header')
-<div class="bg-image" style="background-image: url('{{ asset('page/img/background.png') }}'); height: 80vh">
-  <div class="mask">
-    <div class="d-flex justify-content-center align-items-center h-100">
-      <div class="dropdown-center">
-        <button
-          class="btn btn-light dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Pilih Provinsi
-        </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Sumatera Barat</a></li>
-          <li><a class="dropdown-item" href="#">Sumatera Selatan</a></li>
-          <li><a class="dropdown-item" href="#">Sumatera Utara</a></li>
-        </ul>
-      </div>
+    <div class="bg-image" style="background-image: url('{{ asset('page/img/background.png') }}'); height: 80vh">
+        <div class="mask">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="dropdown-left">
+                    <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Pilih Provinsi
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-start" style="max-height: 200px; overflow-y: auto;">
+                        @foreach ($select as $item)
+                            <li><a class="dropdown-item" href="{{ $item->id }}">{{ $item->nama }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 @endsection
 
 @section('content')
-<section id="trending">
-  <h1 class="text-center my-5">Trending</h1>
-  <div class="container">
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
-            class="card-img-top"
-            alt="Hollywood Sign on The Hill"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content. This content is a
-              little bit longer.
-            </p>
-          </div>
+    <section id="trending">
+        <h1 class="text-center my-5">Trending</h1>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach ($cards as $card)
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="{{ asset('storage/wisata/' . $card->foto) }}" class="card-img-top" alt="Card Image" />
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $card['nama_wisata'] }}</h5>
+                                <p class="card-text">
+                                    {{ Str::limit(strip_tags($card->deskripsi), $limit = 100, $end = '...') }}
+                                    @if (strlen(strip_tags($card->deskripsi)) > 100)
+                                        <a href="{{ route('detail', ['id' => $card->id]) }}">Read
+                                            More</a>
+                                    @endif
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
-            class="card-img-top"
-            alt="Palm Springs Road"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-          </div>
         </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
-            class="card-img-top"
-            alt="Los Angeles Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
-            class="card-img-top"
-            alt="Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content. This content is a
-              little bit longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
-            class="card-img-top"
-            alt="Hollywood Sign on The Hill"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content. This content is a
-              little bit longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
-            class="card-img-top"
-            alt="Palm Springs Road"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
-            class="card-img-top"
-            alt="Los Angeles Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
-            class="card-img-top"
-            alt="Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content. This content is a
-              little bit longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
-            class="card-img-top"
-            alt="Los Angeles Skyscrapers"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a
-              natural lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+    </section>
 @endsection
