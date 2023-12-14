a47b4d9acb6cd2db5648cb68abb59dd947f63a87
aryadhievha03 <aryadhievha03@gmail.com>
2023-12-14 18:22:23 +0700

    [AD - #CSD38]

---
+++ resources/views/page/_partials/footer.blade.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+  <!-- Copyright - Bootstrap Brain Component -->
+  <section class="py-5 py-xl-8 py-xxl-9 border-top border-light">
+    <div class="container overflow-hidden">
+      <div class="row gy-5 gy-md-0">
+        <div class="col-xs-12 col-md-7 order-1 order-md-0">
+          <div class="copyright text-center text-md-start text-white">
+            &copy; 2024. All Rights Reserved.
+          </div>
+          <div
+            class="credits text-secondary text-center text-md-start mt-2 fs-7 text-white"
+          >
+            Built by
+            <a
+              href="https://bootstrapbrain.com/"
+              class="text-white text-decoration-none"
+              >Jelajah Nusantara</a
+            >
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-md-5 order-0 order-md-1">
+          <div class="nav justify-content-center justify-content-md-end">
+            <p class="mb-0 text-center">
+              <a
+                class="text-decoration-none d-flex flex-column align-items-center text-white"
+                href="mailto:TeamJelajahNusantara@gmail.com"
+              >
+                <span>Contact Us</span>
+                <i class="bi bi-envelope-arrow-down"></i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</footer>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -10,7 +10,8 @@
                 <p class="text-app-secondary" style="font-size: 48px;">Jelajah Nusantara</p>
             </div>
         </div>
-        <img class="w-100 h-100" style="object-fit: cover; object-position: center" src="{{ asset('page/img/background-3.png') }}" alt="" srcset="">
+        <img class="w-100 h-100" style="object-fit: cover; object-position: center"
+            src="{{ asset('page/img/background-3.png') }}" alt="" srcset="">
     </div>
 @endsection
 
@@ -18,26 +19,26 @@
     <div class="container">
         <section>
             <div class="row">
-                <div class="col-md-6 gx-5 mb-2">
+                <div class="col-lg-6 gx-5"></div>
+                <div class="col-lg-6 gx-5">
+                    
+                    <h4><strong>Jelajah Nusantara</strong></h4>
+                </div>
+                <div class="col-lg-6 gx-5 mb-2">
                     <div class="bg-image rounded-5">
                         <img src="https://mdbootstrap.com/img/new/slides/031.jpg" class="img-fluid" />
                     </div>
                 </div>
 
-                <div class="col-md-6 gx-5 mb-2">
-                    <h4><strong>Facilis consequatur eligendi</strong></h4>
-                    <p class="text-muted">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                        consequatur eligendi quisquam doloremque vero ex debitis
-                        veritatis placeat unde animi laborum sapiente illo possimus,
-                        commodi dignissimos obcaecati illum maiores corporis.
-                    </p>
-                    <p><strong>Doloremque vero ex debitis veritatis?</strong></p>
-                    <p class="text-muted">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                        itaque voluptate nesciunt laborum incidunt. Officia, quam
-                        consectetur. Earum eligendi aliquam illum alias, unde optio
-                        accusantium soluta, iusto molestiae adipisci et?
+                <div class="col-lg-6 gx-5 mb-2">
+                    <p class="text-muted" style="text-align: justify">
+                        Teman setia petualangan Anda di berbagai destinasi
+                        menakjubkan di Indonesia! Kami lebih dari sekadar aplikasi, kami adalah sahabat perjalanan yang
+                        membimbing Anda menemukan keindahan alam, kekayaan budaya, dan pengalaman tak terlupakan di seluruh
+                        Nusantara. Kami hadir untuk memastikan setiap langkah perjalanan Anda diberkati
+                        dengan keunikan dan keindahan sejarah yang hanya dapat ditemukan di Indonesia.
+                        Temukan keajaiban-keajaiban sejarah yang memikat, dari candi kuno hingga peninggalan kolonial, dan rasakan
+                        pesona Indonesia yang kaya akan warisan budaya. 
                     </p>
                 </div>
             </div>
@@ -49,26 +50,32 @@
         <div class="container">
             <div class="row g-4 mb-4">
                 @forelse ($wisata as $item)
-                <div class="col-md-3">
-                    <a href="{{ route('page.wisata.detail', [$item->id, Str::slug($item->nama_wisata)]) }}" style="color: black; text-decoration: none">
-                        <div class="card h-100">
-                            <img src="{{ asset('storage/wisata/' . $item->foto) }}" class="card-img-top"
-                                alt="Card Image" />
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold">{{ $item->nama_wisata }}</h5>
-                                <div class="px-2 py-1 text-white bg-app-secondary rounded d-inline-block">{{ $item->provinsi->nama }}</div>
-                                <small class="d-block text-muted my-2"><i class="bi bi-person-fill"></i> {{ $item->user->name }}</small>
-                                <small class="d-block text-muted my-2"><i class="bi bi-clock"></i> {{ \Carbon\Carbon::parse($item->created_at)->isoFormat('D MMMM Y') }}</small>
-                                <p class="card-text">
-                                    {{ Str::limit(strip_tags($item->deskripsi), $limit = 100, $end = '...') }}
-                                    @if (strlen(strip_tags($item->deskripsi)) > 100)
-                                        <a href="{{ route('page.wisata.detail', [$item->id, Str::slug($item->nama_wisata)]) }}">Lihat Selengkapnya</a>
-                                    @endif
-                                </p>
+                    <div class="col-md-3">
+                        <a href="{{ route('page.wisata.detail', [$item->id, Str::slug($item->nama_wisata)]) }}"
+                            style="color: black; text-decoration: none">
+                            <div class="card h-100">
+                                <img src="{{ asset('storage/wisata/' . $item->foto) }}" class="card-img-top"
+                                    alt="Card Image" />
+                                <div class="card-body">
+                                    <h5 class="card-title fw-bold">{{ $item->nama_wisata }}</h5>
+                                    <div class="px-2 py-1 text-white bg-app-secondary rounded d-inline-block">
+                                        {{ $item->provinsi->nama }}</div>
+                                    <small class="d-block text-muted my-2"><i class="bi bi-person-fill"></i>
+                                        {{ $item->user->name }}</small>
+                                    <small class="d-block text-muted my-2"><i class="bi bi-clock"></i>
+                                        {{ \Carbon\Carbon::parse($item->created_at)->isoFormat('D MMMM Y') }}</small>
+                                    <p class="card-text">
+                                        {{ Str::limit(strip_tags($item->deskripsi), $limit = 100, $end = '...') }}
+                                        @if (strlen(strip_tags($item->deskripsi)) > 100)
+                                            <a
+                                                href="{{ route('page.wisata.detail', [$item->id, Str::slug($item->nama_wisata)]) }}">Lihat
+                                                Selengkapnya</a>
+                                        @endif
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 @empty
                     <div class="text-center">
                         Belum ada data tersedia
